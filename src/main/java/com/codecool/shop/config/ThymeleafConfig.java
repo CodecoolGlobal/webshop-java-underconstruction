b80c656cc2bab2e79051ba3d26d63c0360c374cf
botond <belgianmaniax@gmail.com>
2020-02-10 13:33:15 +0100

    Add comments to ThymeleafConfig.java

--- src/main/java/com/codecool/shop/config/ThymeleafConfig.java
+++ src/main/java/com/codecool/shop/config/ThymeleafConfig.java
@@ -24,15 +24,50 @@ public class ThymeleafConfig implements ServletContextListener {
     }
 
     private TemplateEngine templateEngine(ServletContext servletContext) {
+        /*
+        Template Engine objects are implementations of the org.thymeleaf.ITemplateEngine
+        interface. One of these implementations is offered by the Thymeleaf core:
+        org.thymeleaf.TemplateEngine.
+
+        A template resolver is the only required parameter a TemplateEngine needs,
+        although there are many others.
+         */
         TemplateEngine engine = new TemplateEngine();
         engine.setTemplateResolver(templateResolver(servletContext));
         return engine;
     }
 
     private ITemplateResolver templateResolver(ServletContext servletContext) {
+        /*
+        Template Resolvers are objects that implement an interface from the
+        Thymeleaf API called org.thymeleaf.templateresolver.ITemplateResolver.
+
+        These objects are in charge of determining how our templates will be accessed.
+
+        The org.thymeleaf.templateresolver.ServletContextTemplateResolver means that
+        we are going to retrieve our template files as resources from the Servlet Context:
+        an application-wide javax.servlet.ServletContext object that exists in every
+        Java web application, and that resolves resources from the web application root.
+         */
         ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(servletContext);
         resolver.setPrefix("/templates/");
+
+        /*
+        HTML is the default template mode for ServletContextTemplateResolver,
+        but it is good practice to establish it anyway.
+         */
         resolver.setTemplateMode(TemplateMode.HTML);
+
+        /*
+        If we did this as well:
+            resolver.setSuffix(".html");
+        then the template name "product/index" would correspond to:
+            servletContext.getResourceAsStream("/templates/product/index.html");
+
+        What the skeleton code has in ProductController.doGet:
+            engine.process("product/index.html", context, resp.getWriter());
+        because this skeleton method doesn't call resolver.setSuffix.
+         */
         return resolver;
     }
 
