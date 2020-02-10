5d8857bd128568a4bea7510ccd72a983c53094f5
botond <belgianmaniax@gmail.com>
2020-02-10 13:43:36 +0100

    Add comments to ProductController.java

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -22,18 +22,27 @@ public class ProductController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Get DAO instances
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
+        // Get TemplateEngine from ServletContext. This ServletContext is used
+        // to create the TemplateEngine instance inside ThymeleafConfig.contextInitialized,
+        // which is then stored as an attribute in ServletContext.
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+
+        // This looks somewhat like magic for now
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
         // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         // context.setVariables(params);
+
+        // Let the engine process the template with the specified context
         engine.process("product/index.html", context, resp.getWriter());
     }
 
