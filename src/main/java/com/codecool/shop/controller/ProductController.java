1b697384522a6bd8e1ccbe01e041da7a7c11edac
Rajnai Máté <ramate.rajnai@gmail.com>
2020-02-12 10:25:22 +0100

    Refactor

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -40,43 +40,28 @@ public class ProductController extends HttpServlet {
         this.engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         this.context = new WebContext(req, resp, req.getServletContext());
 
-        System.out.println("selected supplier: " + req.getParameter("selected-supplier"));
-        // mikor betöltöm az oldalt és még nem filtereztem és submitoltam, akkor még nem létezik a req object és azért lesz null?
-        if (req.getParameter("selected-supplier") == null || req.getParameter("selected-supplier").equals("No filter")) // nem létezik még Or No filter
+        if (req.getParameter("selected-supplier") == null || req.getParameter("selected-supplier").equals("No filter"))
             defaultGet(req, resp);
         else filterBySupplier(req, resp);
+
+        context.setVariable("suppliers", supplierDao.getAll());
+        context.setVariable("page_path", "product/index.html");
+        engine.process("layout.html", context, resp.getWriter());
     }
 
     protected void defaultGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ProductCategory productCategory = productCategoryDao.find(1);
-
-        List<Supplier> suppliers = supplierDao.getAll();
-        context.setVariable("suppliers", suppliers);
-
-
         context.setVariable("category", productCategory);
         context.setVariable("products", productDao.getBy(productCategory));
-        context.setVariable("page_path", "product/index.html");
 
-        engine.process("layout.html", context, resp.getWriter());
     }
 
     protected void filterBySupplier(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int selectedSupplierId = Integer.parseInt(req.getParameter("selected-supplier"));
         context.setVariable("selectedSupplierId", selectedSupplierId);
         context.setVariable("products", productDao.getBy(supplierDao.find(selectedSupplierId)));
-
-        // refactor
-        List<Supplier> suppliers = supplierDao.getAll();
-        context.setVariable("suppliers", suppliers);
         ProductCategory productCategory = productCategoryDao.find(1);
         context.setVariable("category", productCategory);
-        context.setVariable("page_path", "product/index.html");
-        engine.process("layout.html", context, resp.getWriter());
-
-
-        System.out.println("It is filtering");
-
     }
 
 
