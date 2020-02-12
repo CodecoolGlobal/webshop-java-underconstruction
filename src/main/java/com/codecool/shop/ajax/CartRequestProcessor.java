7c5d673e9170cf7f67f4576951f3ca1610b1fdff
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-02-12 14:15:16 +0100

    Add CartRequestProcessor for handlinh request logic

---
+++ src/main/java/com/codecool/shop/ajax/CartRequestProcessor.java
@@ -0,0 +1,45 @@
+package com.codecool.shop.ajax;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.Order;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class CartRequestProcessor implements RequestProcessor{
+    @Override
+    public String extractJson(HttpServletRequest req) {
+        return null;
+    }
+
+    @Override
+    public void defaultResponse(HttpServletResponse resp, HttpServletRequest req) throws IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        HttpSession session = req.getSession(false);
+        Order order = null;
+        OrderDaoMem orderDaoMem = new OrderDaoMem();
+
+        if (session != null) {
+            order = (Order) session.getAttribute("order");
+        }
+        else {
+            order = new Order();
+            orderDaoMem.handleAddItem(order, 1);
+            orderDaoMem.handleAddItem(order, 1);
+            orderDaoMem.handleAddItem(order, 2);
+
+        }
+
+        context.setVariable("order", order);
+        context.setVariable("total_price", order.calculateTotalPrice());
+        context.setVariable("page_path", "cart/cart.html");
+        engine.process("layout.html", context, resp.getWriter());
+    }
+}
