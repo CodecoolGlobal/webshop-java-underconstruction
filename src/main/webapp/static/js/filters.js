90e1aa475cae8b0b01040bbf156bff8f1aac1ce5
botond <belgianmaniax@gmail.com>
2020-02-12 11:18:46 +0100

    Modularize js files

---
+++ src/main/webapp/static/js/filters.js
@@ -0,0 +1,75 @@
+import {Util} from "./util.js";
+import {ApiConnector} from "./api_connector.js";
+import {CardView} from "./dom.js";
+
+export class FilterProvider {
+
+    constructor() {
+        this.productCategoryFilterInstance = new ProductCategoryFilter("product-category-filter-form");
+    }
+
+    get productCategoryFilter() {
+        return this.productCategoryFilterInstance;
+    }
+}
+
+class Filter {
+    constructor(formId) {
+        this.productListContainer = document.getElementById("products");
+        this.productCardViewTemplate = document.querySelector(".product-card-view").cloneNode(true);
+        this.form = document.getElementById(formId);
+        this.selectElement = this.form.querySelector("select");
+        this.button = this.form.querySelector("button");
+    }
+
+    get previouslySelectedId() {
+        const selectedId = this.selectElement.dataset.selectedCategoryId;
+        return (selectedId === "null") ? -1 : Number.parseInt(selectedId);
+    }
+
+    set selectedId(id) {
+        this.selectElement.dataset.selectedCategoryId = id;
+    }
+
+    get selectedId() {
+        const selectedId = this.selectElement.value;
+        if (Util.isNumber(selectedId)) {
+            return Number.parseInt(selectedId);
+        } else {
+            return -1;
+        }
+    }
+
+
+    set buttonHandler(handler) {
+        this.button.addEventListener("click", handler.bind(this));
+    }
+
+    clearContainer() {
+        this.productListContainer.innerHTML = "";
+    }
+
+    processResponse(products) {
+        const cardViews = products.map(p => new CardView(this.productCardViewTemplate, p));
+        this.clearContainer();
+        cardViews.forEach(cardView => cardView.render(this.productListContainer));
+    }
+}
+
+class ProductCategoryFilter extends Filter {
+
+    constructor(formId) {
+        super(formId);
+    }
+
+    handleButtonClick() {
+        const selectedId = this.selectedId;
+        if (selectedId !== this.previouslySelectedId) {
+            ApiConnector._api_get(
+                `/?product_category=${selectedId}`,
+                products => this.processResponse(products)
+            );
+            this.selectedId = selectedId;
+        }
+    }
+}
--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,102 +1,10 @@
-import {ApiConnector} from "./api_connector.js";
-import {Util} from "./util.js";
+import {FilterProvider} from "./filters.js";
 
 class Main {
     static init() {
-        const productCategoryFilter = new ProductCategoryFilter("product-category-filter-form");
+        const filterProvider = new FilterProvider();
+        const productCategoryFilter = filterProvider.productCategoryFilter;
         productCategoryFilter.buttonHandler = productCategoryFilter.handleButtonClick;
-    }
-}
-
-class Filter {
-    constructor(formId) {
-        this.productListContainer = document.getElementById("products");
-        this.productCardViewTemplate = document.querySelector(".product-card-view").cloneNode(true);
-        this.form = document.getElementById(formId);
-        this.selectElement = this.form.querySelector("select");
-        this.button = this.form.querySelector("button");
-    }
-
-    get previouslySelectedId() {
-        const selectedId = this.selectElement.dataset.selectedCategoryId;
-        return (selectedId === "null") ? -1 : Number.parseInt(selectedId);
-    }
-
-    set selectedId(id) {
-        this.selectElement.dataset.selectedCategoryId = id;
-    }
-
-    get selectedId() {
-        const selectedId = this.selectElement.value;
-        if (Util.isNumber(selectedId)) {
-            return Number.parseInt(selectedId);
-        } else {
-            return -1;
-        }
-    }
-
-
-    set buttonHandler(handler) {
-        this.button.addEventListener("click", handler.bind(this));
-    }
-
-    clearContainer() {
-        this.productListContainer.innerHTML = "";
-    }
-
-    processResponse(products) {
-        const cardViews = products.map(p => new CardView(this.productCardViewTemplate, p));
-        this.clearContainer();
-        cardViews.forEach(cardView => cardView.render(this.productListContainer));
-    }
-}
-
-class ProductCategoryFilter extends Filter {
-
-    constructor(formId) {
-        super(formId);
-    }
-
-    handleButtonClick() {
-        const selectedId = this.selectedId;
-        if (selectedId !== this.previouslySelectedId) {
-            ApiConnector._api_get(
-                `/?product_category=${selectedId}`,
-                products => this.processResponse(products)
-            );
-            this.selectedId = selectedId;
-        }
-    }
-}
-
-class CardView {
-    constructor(template, product) {
-        this.template = template.cloneNode(true);
-        this.imageById = product.id;
-        this.name = product.name;
-        this.description = product.description;
-        const price = parseFloat(product.defaultPrice).toFixed(1);
-        this.price = `${price} ${product.defaultCurrency}`;
-    }
-
-    set imageById(id) {
-        this.template.querySelector(".product-image").src = `/static/img/product_${id}.jpg`;
-    }
-
-    set name(name) {
-        this.template.querySelector(".product-name").textContent = name;
-    }
-
-    set description(description) {
-        this.template.querySelector(".product-description").textContent = description;
-    }
-
-    set price(price) {
-        this.template.querySelector(".product-price").textContent = price;
-    }
-
-    render(container) {
-        container.appendChild(this.template);
     }
 }
 
