b06bfdb9d4443028863f97b1b099c42840f8591a
botond <belgianmaniax@gmail.com>
2020-02-12 13:47:18 +0100

    Separate ProductFieldExclusionStrategy

--- src/main/java/com/codecool/shop/ajax/json/FilteredProductJsonProvider.java
+++ src/main/java/com/codecool/shop/ajax/json/FilteredProductJsonProvider.java
@@ -1,11 +1,8 @@
 package com.codecool.shop.ajax.json;
 
 import com.codecool.shop.model.Product;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.Expose;
 
 import java.util.List;
 
@@ -14,23 +11,9 @@ public class FilteredProductJsonProvider {
     private Gson gson;
 
     public FilteredProductJsonProvider() {
-        this.gson = buildProductConverter();
-    }
-
-    private Gson buildProductConverter() {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setExclusionStrategies(new ExclusionStrategy() {
-            @Override
-            public boolean shouldSkipField(FieldAttributes f) {
-                return f.getAnnotation(Expose.class) == null;
-            }
-
-            @Override
-            public boolean shouldSkipClass(Class<?> aClass) {
-                return false;
-            }
-        });
-        return gsonBuilder.create();
+        gsonBuilder.setExclusionStrategies(new ProductFieldExclusionStrategy());
+        this.gson = gsonBuilder.create();
     }
 
     public String provide(List<Product> elements) {
