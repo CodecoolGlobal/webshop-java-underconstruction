fa6740a59efbc471890975f6d853a48aa2f489fa
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-02-12 12:53:38 +0100

    Add basic cartcontroller

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Order;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/cart"})
@@ -20,9 +22,19 @@ public class CartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("page_path", "cart/cart.html");
-        engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        engine.process("layout.html", context, resp.getWriter());
+        this.context = new WebContext(req, resp, req.getServletContext());
+        this.engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+
+        defaultGet(req, resp);
+    }
+
+    private void defaultGet(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession(false);
+        Order order;
+
+        if (session != null) {
+            order = (Order) session.getAttribute("order");
+
+        }
     }
 }
