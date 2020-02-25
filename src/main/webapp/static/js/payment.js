6fc39daa0d79b27edf11b0802951f14ee7d24e84
Rajnai Máté <ramate.rajnai@gmail.com>
2020-02-25 16:33:32 +0100

    Choose payment method and hide not checked payment method

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -20,9 +20,37 @@ class PaymentMethodChooser {
     }
 
     choosePaymentMethodHandler(event) {
+        let target = event.target;
         console.log("HEY");
-
-
+        if (!target.classList.contains("payment-method-checked")) {
+            this.checkboxes.forEach(checkbox => {
+                if (checkbox.isEqualNode(target)) {
+                    checkbox.classList.add("payment-method-checked");
+                    checkbox.classList.remove("payment-method-not-checked");
+                } else {
+                    // REFACTOR - TOGGLE??
+                    checkbox
+                        .parentElement
+                        .parentElement
+                        .parentElement
+                        .classList.add("payment-method-hidden");
+                }
+            })
+        } else {
+            this.checkboxes.forEach(checkbox => {
+                if (checkbox.isEqualNode(target)) {
+                    checkbox.classList.remove("payment-method-checked");
+                    checkbox.classList.add("payment-method-not-checked");
+                } else {
+                    // REFACTOR
+                    checkbox
+                        .parentElement
+                        .parentElement
+                        .parentElement
+                        .classList.remove("payment-method-hidden");
+                }
+            })
+        }
     }
 }
 
