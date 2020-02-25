1453589d746fe66634dd94d00b0df46bf9ac44d7
Rajnai Máté <ramate.rajnai@gmail.com>
2020-02-25 14:15:14 +0100

    Add web context

--- src/main/java/com/codecool/shop/controller/requestprocessing/PaymentRequestProcessor.java
+++ src/main/java/com/codecool/shop/controller/requestprocessing/PaymentRequestProcessor.java
@@ -1,6 +1,10 @@
 package com.codecool.shop.controller.requestprocessing;
 
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +19,9 @@ public class PaymentRequestProcessor implements RequestProcessor {
 
     @Override
     public void defaultResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
 
 
 
