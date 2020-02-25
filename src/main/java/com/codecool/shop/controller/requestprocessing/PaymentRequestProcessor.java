939520c0ea6968c3f2944eda05db1569c42e545c
Rajnai Máté <ramate.rajnai@gmail.com>
2020-02-25 14:16:20 +0100

    Retrieve order from session

--- src/main/java/com/codecool/shop/controller/requestprocessing/PaymentRequestProcessor.java
+++ src/main/java/com/codecool/shop/controller/requestprocessing/PaymentRequestProcessor.java
@@ -2,11 +2,13 @@ package com.codecool.shop.controller.requestprocessing;
 
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Order;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class PaymentRequestProcessor implements RequestProcessor {
@@ -22,6 +24,11 @@ public class PaymentRequestProcessor implements RequestProcessor {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        SessionHandler sessionHandler = new SessionHandler();
+
+        HttpSession session = sessionHandler.getSession(req);
+        Order order = sessionHandler.checkOrderInSession(session);
 
 
 
