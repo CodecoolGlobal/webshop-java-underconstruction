bdabc5e182c0d978d591ce4b538f25bc891c948c
botond <belgianmaniax@gmail.com>
2020-02-26 09:34:00 +0100

    Create enum RequestProcessingStrategy

---
+++ src/main/java/com/codecool/shop/controller/requestprocessing/RequestProcessingStrategy.java
@@ -0,0 +1,8 @@
+package com.codecool.shop.controller.requestprocessing;
+
+public enum RequestProcessingStrategy {
+    DEFAULT,
+    FILTER_PRODUCTS,
+    UPDATE_ORDER,
+    ADD_CUSTOMER_TO_ORDER
+}
