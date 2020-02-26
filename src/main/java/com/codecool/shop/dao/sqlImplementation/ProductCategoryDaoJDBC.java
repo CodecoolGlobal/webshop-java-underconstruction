da1a02f4e3447fe109600b78ab7201442e8977ec
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-02-26 14:14:28 +0100

    Add skeleton of ProductCategoryDaoJDBC

--- src/main/java/com/codecool/shop/dao/sqlImplementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/sqlImplementation/ProductCategoryDaoJDBC.java
@@ -1,11 +1,19 @@
 package com.codecool.shop.dao.sqlImplementation;
 
 import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.data.sql.Executor;
+import com.codecool.shop.data.sql.Extractor;
+import com.codecool.shop.data.sql.ProductCategoryExtractor;
 import com.codecool.shop.model.ProductCategory;
 
 import java.util.List;
 
 public class ProductCategoryDaoJDBC implements ProductCategoryDao {
+
+    private Executor executor = new Executor();
+    private Extractor<ProductCategory> extractor = new ProductCategoryExtractor();
+
+
     @Override
     public void add(ProductCategory category) {
 
