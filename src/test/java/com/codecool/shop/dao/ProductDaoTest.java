3f00490e285b10ce0fa8d1b31ac5e98a73a87147
botond <belgianmaniax@gmail.com>
2020-02-26 14:37:36 +0100

    Test getBy for suppliers and product categories

--- src/test/java/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -1,12 +1,20 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.data.factories.Synthesizer;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import com.codecool.shop.util.Util;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +22,8 @@ class ProductDaoTest {
 
     private static Synthesizer synthesizer = new Synthesizer();
     private static ProductDao productDao = ProductDaoMem.getInstance();
+    private static ProductCategoryDao productCategoryDao = ProductCategoryDaoMem.getInstance();
+    private static SupplierDao supplierDao = SupplierDaoMem.getInstance();
 
     @BeforeAll
     @Test
@@ -21,24 +31,30 @@ class ProductDaoTest {
         synthesizer.synthesize();
     }
 
-    @BeforeEach
-    void logTestNameBeforeEach(TestInfo testInfo) {
-        System.out.println(Util.multiplyString("-", 50));
-        System.out.println(String.format("Initiating %s", testInfo.getDisplayName()));
-        System.out.println(Util.multiplyString("-", 50) + "\n");
-    }
-
-    @AfterEach
-    void logTestNameAfterEach(TestInfo testInfo) {
-        System.out.println("\n" + Util.multiplyString("-", 50));
-        System.out.println(String.format("Finishing %s", testInfo.getDisplayName()));
-        System.out.println(Util.multiplyString("-", 50));
-    }
-
     @Test
     void testGetAllReturnsNotNull() {
         List<Product> products = productDao.getAll();
         assertNotNull(products, "Was null");
+    }
+
+    @ParameterizedTest(name = "id = {arguments}")
+    @ValueSource(ints = {1, 2, 3, 5, 7, 8, 1, 2, 1, 1})
+    void testGetByProductCategory(int categoryId) {
+        ProductCategory productCategory = productCategoryDao.find(categoryId);
+        List<Product> products = productDao.getBy(productCategory);
+        assertTrue(products.stream().allMatch(product -> product.getProductCategory().getId() == categoryId));
+    }
+
+    @ParameterizedTest(name = "id = {arguments}")
+    @MethodSource({"idSupplier"})
+    void testGetBySupplier(int supplierId) {
+        Supplier supplier = supplierDao.find(supplierId);
+        List<Product> products = productDao.getBy(supplier);
+        assertTrue(products.stream().allMatch(product -> product.getSupplier().getId() == supplierId));
+    }
+
+    static IntStream idSupplier() {
+        return IntStream.generate(() -> Util.randRange(0, 10)).limit(10);
     }
 
 }
--- src/test/java/com/codecool/shop/util/Util.java
+++ src/test/java/com/codecool/shop/util/Util.java
@@ -1,12 +1,19 @@
 package com.codecool.shop.util;
 
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Util {
+
+    private static Random random = new Random();
 
     public static String multiplyString(String s, int by) {
         StringBuilder sb = new StringBuilder();
         IntStream.rangeClosed(1, by).forEach(i -> sb.append(s));
         return sb.toString();
+    }
+
+    public static int randRange(int start, int end) {
+        return start + random.nextInt(end);
     }
 }
