ec8e202d89ec96b0a579511e8113fe1444e3b731
botond <belgianmaniax@gmail.com>
2020-02-26 09:48:26 +0100

    Create class AbstractRequestProcessor

---
+++ src/main/java/com/codecool/shop/controller/requestprocessing/AbstractRequestProcessor.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.controller.requestprocessing;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public abstract class AbstractRequestProcessor implements IRequestProcessor {
+
+    AbstractRequestProcessor() {
+    }
+
+    abstract void defaultResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException;
+
+    void sendJson(HttpServletResponse resp, String json) throws IOException {
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        out.print(json);
+    }
+
+    public boolean filteredFaviconRequest(HttpServletRequest req, HttpServletResponse resp) {
+        if ("/favicon.ico".equals(req.getRequestURI())) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
