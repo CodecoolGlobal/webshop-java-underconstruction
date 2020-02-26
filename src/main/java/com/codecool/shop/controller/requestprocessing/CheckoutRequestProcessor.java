1ac1ee29fb1ea8e41ad5cdf9d3c25edb8fb5b6a6
botond <belgianmaniax@gmail.com>
2020-02-26 10:45:11 +0100

    Refactor CheckoutRequestProcessor and CheckoutController

--- src/main/java/com/codecool/shop/controller/requestprocessing/CheckoutRequestProcessor.java
+++ src/main/java/com/codecool/shop/controller/requestprocessing/CheckoutRequestProcessor.java
@@ -12,34 +12,38 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-public class CheckoutRequestProcessor implements RequestProcessor {
+public class CheckoutRequestProcessor extends AbstractRequestProcessor {
 
     private CheckoutRequestJsonConverter jsonConverter = new CheckoutRequestJsonConverter();
+    private SessionHandler sessionHandler = new SessionHandler();
 
     @Override
-    public String extractJson(HttpServletRequest req) {
-        return null;
+    public void digestRequest(HttpServletRequest req, HttpServletResponse resp, RequestProcessingStrategy strategy) throws IOException {
+        switch (strategy) {
+            case DEFAULT:
+                defaultResponse(req, resp);
+                break;
+            case ADD_CUSTOMER_TO_ORDER:
+                addCustomerToOrder(req, resp);
+                break;
+        }
     }
 
     @Override
-    public void defaultResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    void defaultResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("page_path", "checkout/checkout.html");
         engine.process("layout.html", context, resp.getWriter());
     }
 
-    @Override
-    public void manipulateDao(HttpServletRequest req) {
-
-    }
-
-    public String addCustomerToOrder(HttpServletRequest req, SessionHandler sessionHandler) throws IOException {
+    void addCustomerToOrder(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Optional<Customer> optionalCustomer = jsonConverter.parseCustomer(req);
         optionalCustomer.ifPresent(customer ->  {
             Order order = sessionHandler.getOrderFromSession(req);
             order.setCustomer(optionalCustomer.get());
         });
-        return jsonConverter.parsingSuccessJson(optionalCustomer.isPresent());
+        String json = jsonConverter.parsingSuccessJson(optionalCustomer.isPresent());
+        sendJson(resp, json);
     }
 }
