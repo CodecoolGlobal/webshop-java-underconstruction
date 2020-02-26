809b8ecdcbf3f6e8c510b06a21de3065972fe6c6
botond <belgianmaniax@gmail.com>
2020-02-26 10:43:25 +0100

    Remove empty lines

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -21,14 +21,11 @@ public class CartController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         requestProcessor.digestRequest(req, resp, DEFAULT);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         requestProcessor.digestRequest(req, resp, UPDATE);
-
     }
 }
