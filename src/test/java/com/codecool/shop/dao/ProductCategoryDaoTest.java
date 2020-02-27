27b710e61f81b85e1306236afdee9d37673dd692
botond <belgianmaniax@gmail.com>
2020-02-27 12:33:50 +0100

    Implement ProductCategoryDaoTest.testAdd

--- src/test/java/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -2,8 +2,15 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.data.factories.Synthesizer;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.util.Util;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,8 +25,11 @@ class ProductCategoryDaoTest {
         synthesizer.synthesize();
     }
 
-    @Test
-    void testAdd() {
+    @ParameterizedTest(name = "{displayName}")
+    @MethodSource("categorySupplier")
+    void testAdd(ProductCategory category) {
+        assertDoesNotThrow(() -> productCategoryDao.add(category));
+        assertNotNull(productCategoryDao.find(category.getId()));
     }
 
     @Test
@@ -32,5 +42,10 @@ class ProductCategoryDaoTest {
 
     @Test
     void testGetAll() {
+    }
+
+    static Stream<Arguments> categorySupplier() {
+        ProductCategory category = new ProductCategory(Util.randRange(11, 100), "c", "d", "d");
+        return Stream.of(Arguments.of(category));
     }
 }
