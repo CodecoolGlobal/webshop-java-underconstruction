6754663431d059a7047140a6143695093685dcb0
botond <belgianmaniax@gmail.com>
2020-02-27 13:26:15 +0100

    Re-implement ProductFilteringStrategy

--- src/main/java/com/codecool/shop/controller/requestprocessing/filtering/ProductFilteringStrategy.java
+++ src/main/java/com/codecool/shop/controller/requestprocessing/filtering/ProductFilteringStrategy.java
@@ -1,17 +1,21 @@
 package com.codecool.shop.controller.requestprocessing.filtering;
 
-import com.codecool.shop.model.BaseModel;
-import com.codecool.shop.model.Filterable;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ProductFilteringStrategy {
 
-    private HashMap<String, Integer> filterMap = new HashMap<>();
+    private HashMap<String, Integer[]> filterMap = new HashMap<>();
 
     public ProductFilteringStrategy(HttpServletRequest req) {
+        req.getParameterMap().forEach((key, value) -> filterMap.put(
+                key,
+                (Integer[]) Arrays.stream(value).map(Integer::parseInt).toArray()
+        ));
+    }
 
+    public HashMap<String, Integer[]> getFilterMap() {
+        return filterMap;
     }
 }
