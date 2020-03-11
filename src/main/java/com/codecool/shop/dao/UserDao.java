123173ecd4685d2efdf87121067c8a883221f3ae
botond <belgianmaniax@gmail.com>
2020-03-11 20:49:15 +0100

    Add SecurityException to UserDao.add throws list

--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -3,7 +3,7 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.User;
 
 public interface UserDao {
-    User add(User user);
+    User add(User user) throws SecurityException;
     User find(int id);
     void remove(int id);
     User getBy(String username);
