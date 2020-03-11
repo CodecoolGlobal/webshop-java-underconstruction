277e651ee96db90625a98a1bab91b1bb6679d4ee
botond <belgianmaniax@gmail.com>
2020-03-11 11:26:54 +0100

    Create submitter object

--- src/main/webapp/static/js/registration.js
+++ src/main/webapp/static/js/registration.js
@@ -4,24 +4,36 @@ export const registration = {
     }
 };
 
+const submitter = {
+
+    init: function() {
+
+    },
+
+    get submitButton() {
+        return document.querySelector("#registration-modal .modal-footer-submit");
+    },
+};
+
 const closeButtons = {
-    get headerCloseButton() {
-        return document.querySelector("#registration-modal .modal-header-close");
-    },
-
-    get footerCloseButton() {
-        return document.querySelector("#registration-modal .modal-footer-close");
-    },
-
-    get allButtons() {
-        return [this.headerCloseButton, this.footerCloseButton];
-    },
 
     // 'this' is bound to closeButtons
     init: function() {
         for (const button of this.allButtons) {
             button.addEventListener("click", this.clearValidityReport);
         }
+    },
+
+    get allButtons() {
+        return [this.headerCloseButton, this.footerCloseButton];
+    },
+
+    get headerCloseButton() {
+        return document.querySelector("#registration-modal .modal-header-close");
+    },
+
+    get footerCloseButton() {
+        return document.querySelector("#registration-modal .modal-footer-close");
     },
 
     // 'this' is bound to closeButtons
