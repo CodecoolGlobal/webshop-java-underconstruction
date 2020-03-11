f4021ea5bd37a7ff2c42f38c685a6ea249616812
botond <belgianmaniax@gmail.com>
2020-03-11 19:24:02 +0100

    Extend RequestJsonConverter as concrete class

--- src/main/java/com/codecool/shop/controller/requestprocessing/ajax/RequestJsonConverter.java
+++ src/main/java/com/codecool/shop/controller/requestprocessing/ajax/RequestJsonConverter.java
@@ -1,10 +1,13 @@
 package com.codecool.shop.controller.requestprocessing.ajax;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public abstract class RequestJsonConverter {
+public class RequestJsonConverter {
 
     public String readJson(HttpServletRequest req) throws IOException {
         StringBuilder jsonBuilder = new StringBuilder();
@@ -14,6 +17,21 @@ public abstract class RequestJsonConverter {
             jsonBuilder.append(line);
         }
         return jsonBuilder.toString();
+    }
+
+    public <T> T parse(HttpServletRequest req, Class<T> tClass) throws IOException {
+        return parse(req, tClass, new Gson());
+    }
+
+    public <T> T parse(HttpServletRequest req, Class<T> tClass, Gson gson) throws IOException {
+        String json = readJson(req);
+        T obj = null;
+        try {
+            obj = gson.fromJson(json, tClass);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 
     public String parsingSuccessJson(boolean success) {
