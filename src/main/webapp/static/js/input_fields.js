c83b9c2ca964f133733ac8180fe84634e58b8d03
botond <belgianmaniax@gmail.com>
2020-03-11 22:05:20 +0100

    Delete empty InputField subclasses

--- src/main/webapp/static/js/input_fields.js
+++ src/main/webapp/static/js/input_fields.js
@@ -1,27 +1,7 @@
 export class InputFieldReader {
 
     static getInputField(fieldId) {
-        switch (fieldId) {
-            case "first-name":
-            case "last-name":
-                return new NameInputField(fieldId);
-            case "email":
-                return new EmailInputField(fieldId);
-            case "phone-number":
-                return new PhoneNumberInputField(fieldId);
-            case "billing-country":
-            case "shipping-country":
-                return new CountryInputField(fieldId);
-            case "billing-city":
-            case "shipping-city":
-                return new CityInputField(fieldId);
-            case "billing-zip-code":
-            case "shipping-zip-code":
-                return new ZipCodeInputField(fieldId);
-            case "billing-address":
-            case "shipping-address":
-                return new AddressInputField(fieldId);
-        }
+        return new InputField(fieldId);
     }
 
     static getFieldValue(id) {
@@ -45,33 +25,4 @@ class InputField {
     validate() {
         return this.field.checkValidity();
     }
-
-}
-
-class NameInputField extends InputField {
-
-}
-
-class EmailInputField extends InputField {
-
-}
-
-class PhoneNumberInputField extends InputField {
-
-}
-
-class CountryInputField extends InputField {
-
-}
-
-class CityInputField extends InputField {
-
-}
-
-class ZipCodeInputField extends InputField {
-
-}
-
-class AddressInputField extends InputField {
-
 }
