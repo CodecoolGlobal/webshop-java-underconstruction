d782c374ae87ae7ff7eda256ffd0134a0590c3b5
botond <belgianmaniax@gmail.com>
2020-03-12 13:47:05 +0100

    Delete unused ModalCloser function object

--- src/main/webapp/static/js/modals.js
+++ src/main/webapp/static/js/modals.js
@@ -16,25 +16,3 @@ export const modalCloser = {
         this.form.reset();
     }
 };
-
-export function ModalCloser(modalName) {
-
-    this.setModal = function (modalName) {
-        this.modal = document.getElementById(`${modalName}-modal`);
-        this.form = document.getElementById(`${modalName}-form`);
-    };
-
-    this.setModal(modalName);
-
-    this.getCloseButtons = function() {
-        return Array.from(this.modal.querySelectorAll(".modal-header-close, .modal-footer-close"));
-    };
-
-    this.clearValidityReport = function () {
-        this.form.reset();
-    }.bind(this);
-
-    this.init = function () {
-        this.getCloseButtons().forEach(button => button.addEventListener("click", this.clearValidityReport));
-    };
-}
