f2a8dc3e73e33fd1b902cd6b2fe1b043703a5030
botond <belgianmaniax@gmail.com>
2020-03-13 00:31:26 +0100

    Format code in checkout.js

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -12,17 +12,17 @@ class Main {
         ];
 
         inputFieldIdCollection.forEach(fieldId => container.addField(InputFieldReader.getInputField(fieldId)));
-        const checkoutButton =  document.getElementById("checkout-submit");
+        const checkoutButton = document.getElementById("checkout-submit");
         checkoutButton.addEventListener("click", () => {
-                container.validateFields();
-                if (container.inputsValid) {
-                    const cred = OrderCredentialsCollector.collectData();
-                    ApiConnector._api_post("/checkout", cred, resp => {
-                        console.log(resp.result);
-                        checkoutButton.setAttribute("onclick", location.href="/payment");
-                    });
-                }
-            });
+            container.validateFields();
+            if (container.inputsValid) {
+                const cred = OrderCredentialsCollector.collectData();
+                ApiConnector._api_post("/checkout", cred, resp => {
+                    console.log(resp.result);
+                    checkoutButton.setAttribute("onclick", location.href = "/payment");
+                });
+            }
+        });
 
     }
 }
