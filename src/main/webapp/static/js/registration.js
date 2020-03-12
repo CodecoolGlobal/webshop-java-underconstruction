f023773ee31061b079c6e2076a7005a209098403
botond <belgianmaniax@gmail.com>
2020-03-12 13:28:30 +0100

    Use submitterBase for registration

--- src/main/webapp/static/js/registration.js
+++ src/main/webapp/static/js/registration.js
@@ -1,42 +1,20 @@
 import {ApiConnector} from "./api_connector.js";
 import {ModalCloser} from "./modals.js";
+import {submitterBase} from "./submitter.js";
 
 export const registration = {
     init: function () {
         new ModalCloser("registration").init();
-        new Submitter().init();
+        createRegistrationSubmitter().init();
     }
 };
 
-function Submitter() {
+function createRegistrationSubmitter() {
+    const registrationSubmitter = Object.create(submitterBase);
+    registrationSubmitter.setAction("registration");
+    registrationSubmitter.passwordConfirmation = document.getElementById("registration-password-confirmation");
 
-    this.url = "/user?action=registration";
-
-    this.init = function () {
-        this.getSubmitButton().addEventListener("click", this.submit);
-    };
-
-    this.getSubmitButton = function () {
-        return document.querySelector("#registration-modal .modal-footer-submit");
-    };
-
-    this.getAllInputs = function () {
-        return Array.from(document.querySelectorAll("#registration-form input"));
-    };
-
-    this.getUsername = function () {
-        return document.getElementById("registration-username");
-    };
-
-    this.getPassword = function () {
-        return document.getElementById("registration-password");
-    };
-
-    this.getPasswordConfirmation = function () {
-        return document.getElementById("registration-password-confirmation");
-    };
-
-    this.submit = function () {
+    registrationSubmitter.submit = function () {
         if (!this.validateByHtmlAttributes() || !this.validatePasswordConfirmation()) {
             return;
         }
@@ -45,45 +23,23 @@ function Submitter() {
             const {errorMessage, user} = json;
             this.validateUsernameUniqueness(errorMessage);
         })
-    }.bind(this);
+    }.bind(registrationSubmitter);
 
-    this.validateByHtmlAttributes = function () {
-        return this.getAllInputs().every(input => {
-            const result = input.checkValidity();
-            if (result === false) {
-                input.reportValidity();
-            }
-            return result;
-        });
-    };
-
-    this.validatePasswordConfirmation = function () {
-        const pwConfirmation = this.getPasswordConfirmation();
-        const result = this.getPassword().value === pwConfirmation.value;
+    registrationSubmitter.validatePasswordConfirmation = function () {
+        const result = this.password.value === this.passwordConfirmation.value;
         if (result === false) {
-            this.publishCustomValidity(pwConfirmation, "Please confirm your password correctly.");
+            this.publishCustomValidity(this.passwordConfirmation, "Please confirm your password correctly.");
         }
         return result;
-    };
+    }.bind(registrationSubmitter);
 
-    this.collectData = function () {
-        return {
-            username: this.getUsername().value,
-            password: this.getPassword().value
-        }
-    };
-
-    this.validateUsernameUniqueness = function (errorMessage) {
+    registrationSubmitter.validateUsernameUniqueness = function (errorMessage) {
         const result = errorMessage === null;
         if (result === false) {
-            this.publishCustomValidity(this.getUsername(), errorMessage);
+            this.publishCustomValidity(this.username, errorMessage);
         }
         return result;
-    };
+    }.bind(registrationSubmitter);
 
-    this.publishCustomValidity = function (input, message) {
-        input.setCustomValidity(message);
-        input.reportValidity();
-        input.setCustomValidity("");
-    }
+    return registrationSubmitter;
 }
