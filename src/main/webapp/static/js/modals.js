47e1061f43f6b1c338eb6d828de5bb3d560e97ae
botond <belgianmaniax@gmail.com>
2020-03-12 13:43:41 +0100

    Implement modalCloser as js object

--- src/main/webapp/static/js/modals.js
+++ src/main/webapp/static/js/modals.js
@@ -1,3 +1,22 @@
+export const modalCloser = {
+    init() {
+        this.closeButtons.forEach(button => button.addEventListener("click", this.clearValidityReport.bind(this)));
+    },
+
+    setAction(action) {
+        this.modal = document.getElementById(`${action}-modal`);
+        this.form = document.getElementById(`${action}-form`);
+    },
+
+    get closeButtons() {
+        return Array.from(this.modal.querySelectorAll(".modal-header-close, .modal-footer-close"));
+    },
+
+    clearValidityReport() {
+        this.form.reset();
+    }
+};
+
 export function ModalCloser(modalName) {
 
     this.setModal = function (modalName) {
